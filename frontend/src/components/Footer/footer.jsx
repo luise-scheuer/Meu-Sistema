3ea97fb1bb6148dce9d71b6ec3e1f1b4d63fa1d7
Juan Konrado <juankonradoobregon2007@gmail.com>
2025-06-28 20:43:12 -0300

    mais css

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer>
             <div>
-                <h1>footer</h1>
+               
             </div>
         </footer>
     )
--- frontend/src/components/NavBar/navBar.jsx
+++ frontend/src/components/NavBar/navBar.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
 import "./navBar.css"
+import logo from './logo.png';
 export default function NavBar() {
     return (
         <nav className="navbar">
-            <Link to="/">HOME imagem logo?</Link>
-            <div id="campos"> 
+            <div id="logo">
+                <img src={logo} alt="" />
+            </div>
+            <div id="campos">
                 <Link to="/">Profissional</Link>
                 <Link to="/paciente">Paciente</Link>
                 <Link to="/">Especialidade</Link>
                 <Link to="/">Atendimento</Link>
             </div>
         </nav>
-        
-       
+
+
     )
 }
